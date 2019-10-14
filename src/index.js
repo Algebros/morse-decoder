44c5a4b424b984fc9db831c7f2e20b6609c0c16c
Sofroniy <Sofroniy.m@gmail.com>
2019-10-14 16:38:25 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,52 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let chunks = divider(expr, 10);
+    let morsePhraseArr = convertToMorse(chunks);
+    let arr = [];
+    morsePhraseArr.forEach(element => {
+        if(MORSE_TABLE.hasOwnProperty(element)) {
+            arr.push(MORSE_TABLE[element]);
+        } else {
+            arr.push(" ");
+        }
+    });
+    return arr.join("");
+}
+
+function convertToMorse(chunks) {
+    let arrMorse = [];
+    for (let i = 0; i < chunks.length; i++) {
+        let arr = divider(chunks[i], 2);
+        arr.forEach((currentValue, index) => {
+            switch (currentValue) {
+                case "00":
+                    delete arr[index];
+                    break;
+                case "**":
+                    arr[index] = " ";
+                    break;
+                case "10":
+                    arr[index] = ".";
+                    break;
+                case "11":
+                    arr[index] = "-";
+                    break;
+                default:
+                    break;
+            }
+        }); 
+        arrMorse.push(arr.join(""));
+    }
+    return arrMorse;
+}
+
+function divider(str, divide) {
+    var chunks = [];
+    for (var i = 0; i < str.length; i += divide) {
+        chunks.push(str.substring(i, i + divide));
+    }
+    return chunks;
 }
 
 module.exports = {
